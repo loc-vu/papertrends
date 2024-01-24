dccb440461693ea07748e69d12cce2ba08051b29
loc-vu <vhploc@gmail.com>
2024-01-23 20:39:32 -0800

    initial setup django project

---
+++ arxiv_trends/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for arxiv_trends project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arxiv_trends.settings")
+
+application = get_wsgi_application()
